e60e7f43151d85d18d2b98d925c223970cf742bd
Your Name <you@example.com>
2019-12-04 18:08:53 +0000

    Extracted some functions / added SellIn test

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,10 +9,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!isAgedBrie(item.name)
-                    && !isBackstagePass(item.name)) {
+            if (!isAgedBrie(item)
+                    && !isBackstagePass(item)) {
                 if (item.quality > 0) {
-                    if (!isSulfuras(item.name)) {
+                    if (!isSulfuras(item)) {
                         item.quality = item.quality - 1;
                     }
                 }
@@ -20,31 +20,24 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (isBackstagePass(item.name)) {
+                    if (isBackstagePass(item)) {
                         if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                            increaseQuality(item);
                         }
 
                         if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                            increaseQuality(item);
                         }
                     }
                 }
             }
-
-            if (!isSulfuras(item.name)) {
-                item.sellIn = item.sellIn - 1;
-            }
+            endDay(item);
 
             if (item.sellIn < 0) {
-                if (!isAgedBrie(item.name)) {
-                    if (!isBackstagePass(item.name)) {
+                if (!isAgedBrie(item)) {
+                    if (!isBackstagePass(item)) {
                         if (item.quality > 0) {
-                            if (!isSulfuras(item.name)) {
+                            if (!isSulfuras(item)) {
                                 item.quality = item.quality - 1;
                             }
                         }
@@ -52,24 +45,52 @@ class GildedRose {
                         item.quality = 0;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    increaseQuality(item);
                 }
             }
         }
     }
 
-    public boolean isAgedBrie(String name){
-        return name.equals("Aged Brie");
+    public boolean isAgedBrie(Item item){
+        return item.name.equals("Aged Brie");
     }
 
-    public boolean isSulfuras(String name){
-        return name.equals("Sulfuras, Hand of Ragnaros");
+    public boolean isSulfuras(Item item){
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
 
-    public boolean isBackstagePass(String name){
-        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    public boolean isBackstagePass(Item item){
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    public void increaseQuality(Item item){
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    public void decreaseQuality(Item item){
+        if( (!isAgedBrie(item) || !isBackstagePass(item)
+                || !isSulfuras(item)) && item.quality > 0 ){
+            item.quality = item.quality - 1;
+        }
+    }
+
+    public void endDay(Item item){
+        if(!isSulfuras(item)){
+            item.sellIn -= 1;
+        }
+    }
+
+    public void normalItemQualityDecay(Item item) {
+        if (!isAgedBrie(item)
+                && !isBackstagePass(item)) {
+            if (item.quality > 0) {
+                if (!isSulfuras(item)) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        }
     }
 
 }
