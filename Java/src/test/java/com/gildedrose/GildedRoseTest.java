fcd6f40f18effd3f2fe10cdf2d23fc53392a63e0
Victor Copindean <victor.copindean@gmail.com>
2019-12-04 11:52:50 +0000

    Added first tests for suite 75% coverage

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,67 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    Item[] items;
+    GildedRose app;
+
+    @BeforeEach
+    void setUp() {
+        items = new Item[] {
+                new Item("NormalItem", 5, 50),
+                new Item("Aged Brie", 10, 30),
+                new Item("Sulfuras, Hand of Ragnaros", 0,80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 20, 10),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)};
+        app = new GildedRose(items);
+    }
+
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void testNormalItem() {
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(4,app.items[0].sellIn);
+        assertEquals(49, app.items[0].quality);
+    }
+
+    @Test
+    void testAgedBrie() {
+        app.updateQuality();
+        assertEquals(9, app.items[1].sellIn);
+        assertEquals(31, app.items[1].quality);
+    }
+
+    @Test
+    void testSulfuras() {
+        app.updateQuality();
+        assertEquals(0, app.items[2].sellIn);
+        assertEquals(80, app.items[2].quality);
+    }
+
+    @Test
+    void testBackstagePassGreaterThanTenDays() {
+        app.updateQuality();
+        assertEquals(19, app.items[3].sellIn);
+        assertEquals(11, app.items[3].quality);
+    }
+
+    @Test
+    void testBackstagePassTenDaysOrLower() {
+        app.updateQuality();;
+        assertEquals(9, app.items[4].sellIn);
+        assertEquals(12, app.items[4].quality);
+    }
+
+    @Test
+    void testBackstagePassFiveDaysOrLower() {
+        app.updateQuality();
+        assertEquals(4, app.items[5].sellIn);
+        assertEquals(13, app.items[5].quality);
     }
 
 }
