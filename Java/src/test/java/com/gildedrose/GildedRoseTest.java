0dbbe52880d879336062e3d7cf0c92331105bb29
Your Name <you@example.com>
2019-12-18 15:47:45 +0000

    remove redundant test names

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,161 +10,161 @@ class GildedRoseTest {
     GildedRose app;
 
     @Test
-    void test_NormalItem_QualityDecrease_PozSellIn_PozQuality() {
+    void NormalItem_QualityDecrease_PozSellIn_PozQuality() {
         createItemWithSellInAndQuality("Normal Item", 10, 20);
         app.updateQuality();
         resultingQualityShouldBe(19);
     }
 
     @Test
-    void test_NormalItem_QualityDecrease_NegSellIn_PozQuality(){
+    void NormalItem_QualityDecrease_NegSellIn_PozQuality(){
         createItemWithSellInAndQuality("Normal Item", -7, 30);
         app.updateQuality();
         resultingQualityShouldBe(28);
     }
 
     @Test
-    void test_NormalItem_QualityDecrease_ZeroSellIn_PozQuality(){
+    void NormalItem_QualityDecrease_ZeroSellIn_PozQuality(){
         createItemWithSellInAndQuality("Normal Item", 0, 30);
         app.updateQuality();
         resultingQualityShouldBe(28);
     }
 
     @Test
-    void test_Sulfuras_Quality_SellInZero(){
+    void Sulfuras_Quality_SellInZero(){
         createItemWithSellInAndQuality("Sulfuras, Hand of Ragnaros",0, 80);
         app.updateQuality();
         resultingQualityShouldBe(80);
     }
 
     @Test
-    void test_Sulfuras_Quality_NegSellIn(){
+    void Sulfuras_Quality_NegSellIn(){
         createItemWithSellInAndQuality("Sulfuras, Hand of Ragnaros", -1, 80);
         app.updateQuality();
         resultingQualityShouldBe(80);
     }
 
     @Test
-    void test_Sulfuras_Quality_PozSellIn() {
+    void Sulfuras_Quality_PozSellIn() {
         createItemWithSellInAndQuality("Sulfuras, Hand of Ragnaros", 1, 80);
         app.updateQuality();
         resultingQualityShouldBe(80);
     }
 
     @Test
-    void test_Sulfuras_SellInDecrease(){
+    void Sulfuras_SellInDecrease(){
         createItemWithSellInAndQuality("Sulfuras, Hand of Ragnaros", 5, 80);
         app.updateQuality();
         resultingQualityShouldBe(80);
     }
 
     @Test
-    void test_BackstagePass_QualityIncrease_SellInGreaterThenTen() {
+    void BackstagePass_QualityIncrease_SellInGreaterThenTen() {
         createItemWithSellInAndQuality("Backstage passes to a TAFKAL80ETC concert", 20, 40);
         app.updateQuality();
         resultingQualityShouldBe(41);
     }
 
     @Test
-    void test_BackstagePass_QualityIncrease_SellInLowerThanTen() {
+    void BackstagePass_QualityIncrease_SellInLowerThanTen() {
         createItemWithSellInAndQuality("Backstage passes to a TAFKAL80ETC concert", 8, 40);
         app.updateQuality();
         resultingQualityShouldBe(42);
     }
 
     @Test
-    void test_BackstagePass_QualityIncrease_SellInLowerThanFive() {
+    void BackstagePass_QualityIncrease_SellInLowerThanFive() {
         createItemWithSellInAndQuality("Backstage passes to a TAFKAL80ETC concert", 3, 40);
         app.updateQuality();
         resultingQualityShouldBe(43);
     }
 
     @Test
-    void test_BackstagePass_QualityDecrease_AfterSellIn() {
+    void BackstagePass_QualityDecrease_AfterSellIn() {
         createItemWithSellInAndQuality("Backstage passes to a TAFKAL80ETC concert", -1, 40);
         app.updateQuality();
         resultingQualityShouldBe(0);
     }
 
     @Test
-    void test_BackstagePass_QualityDecrease_OnSellIn(){
+    void BackstagePass_QualityDecrease_OnSellIn(){
         createItemWithSellInAndQuality("Backstage passes to a TAFKAL80ETC concert", 0, 40);
         app.updateQuality();
         resultingQualityShouldBe(0);
     }
 
     @Test
-    void test_AgedBrie_QualityIncrease_PosSellIn_PosQuality() {
+    void AgedBrie_QualityIncrease_PosSellIn_PosQuality() {
         createItemWithSellInAndQuality("Aged Brie", 10, 40);
         app.updateQuality();
         resultingQualityShouldBe(41);
     }
 
     @Test
-    void test_AgedBrie_QualityIncrease_PosSellIn_ZeroQuality(){
+    void AgedBrie_QualityIncrease_PosSellIn_ZeroQuality(){
         createItemWithSellInAndQuality("Aged Brie", 10, 0);
         app.updateQuality();
         resultingQualityShouldBe(1);
     }
 
     @Test
-    void test_AgedBrie_QualityIncrease_ZeroSellIn_PosQuality(){
+    void AgedBrie_QualityIncrease_ZeroSellIn_PosQuality(){
         createItemWithSellInAndQuality("Aged Brie", 0, 20);
         app.updateQuality();
         resultingQualityShouldBe(22);
     }
 
     @Test
-    void test_AgedBrie_QualityIncrease_ZeroSellIn_ZeroQuality(){
+    void AgedBrie_QualityIncrease_ZeroSellIn_ZeroQuality(){
         createItemWithSellInAndQuality("Aged Brie", 0, 0);
         app.updateQuality();
        resultingQualityShouldBe(2);
     }
 
     @Test
-    void test_AgedBrie_QualityIncrease_NegSellIn_ZeroQuality(){
+    void AgedBrie_QualityIncrease_NegSellIn_ZeroQuality(){
         createItemWithSellInAndQuality("Aged Brie", -1, 0);
         app.updateQuality();
         resultingQualityShouldBe(2);
     }
 
     @Test
-    void test_AgedBrie_QualityIncrease_NegSellIn_PosQuality(){
+    void AgedBrie_QualityIncrease_NegSellIn_PosQuality(){
         createItemWithSellInAndQuality("Aged Brie", -1, 20);
         app.updateQuality();
         resultingQualityShouldBe(22);
     }
 
     @Test
-    void test_ConjuredItem_QualityDecrease_PosSellIn_PosQuality(){
+    void ConjuredItem_QualityDecrease_PosSellIn_PosQuality(){
         createItemWithSellInAndQuality("Conjured Item", 10, 20);
         app.updateQuality();
         resultingQualityShouldBe(18);
     }
 
     @Test
-    void test_ConjuredItem_QualityDecrease_ZeroSellIn_PosQuality(){
+    void ConjuredItem_QualityDecrease_ZeroSellIn_PosQuality(){
         createItemWithSellInAndQuality("Conjured Item", 0, 20);
         app.updateQuality();
         resultingQualityShouldBe(16);
     }
 
     @Test
-    void test_ConjuredItem_QualityDecrease_NegSellIn_PosQuality(){
+    void ConjuredItem_QualityDecrease_NegSellIn_PosQuality(){
         createItemWithSellInAndQuality("Conjured Item", -5, 20);
         app.updateQuality();
         resultingQualityShouldBe(16);
     }
 
     @Test
-    void test_Quality_IsNever_Under_Zero(){
+    void Quality_IsNever_Under_Zero(){
         createItemWithSellInAndQuality("Normal Item", 10, 0);
         app.updateQuality();
         resultingQualityShouldBe(0);
     }
 
     @Test
-    void test_Quality_IsNever_Over50(){
+    void Quality_IsNever_Over_50(){
         createItemWithSellInAndQuality("Aged Brie", 5, 50);
         app.updateQuality();
         resultingQualityShouldBe(50);
